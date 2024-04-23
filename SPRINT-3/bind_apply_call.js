e33dd77c6a92a7a18102f86aa1b38f98359c7226
Adarsh Ranjan <104624542+Adarsh0Ranjan@users.noreply.github.com>
2024-04-23 10:26:00 +0530

    SPRINT 3 added

---
+++ SPRINT-3/bind_apply_call.js
@@ -0,0 +1,41 @@
+let cap = {
+    name: "Steve",
+    team: "Cap",
+
+    petersTeam: function (mem1, mem2) {
+        console.log(`Hey ${this.name} I am your neighborhood's  
+        spiderman and i belong to ${this.team}'s team with members  ${mem1} and ${mem2}`);
+    }
+}
+let ironMan = {
+    name: "Tony",
+    team: "Iron Man",
+}
+
+// // borrow the method only once with defined number of param 
+// // use petersTeam method -> only once 
+// cap.petersTeam.call(ironMan,"thor", "loki");
+
+// /****
+//  * 
+//  * Apply: borrow the method only once with n no number of param 
+//  * **/
+let membersArray = ["thor", "loki", "Rajneesh", "Sudheer", "Tapaswini"]
+// cap.petersTeam.apply(ironMan, membersArray);
+
+/*****
+ * bind : making permanent copy of that method to use multiple times
+ * ******/
+const boundFn = cap.petersTeam.bind(ironMan);
+boundFn(membersArray, "Roman")   
+                                                                          
+// Guess the answer.-------------------------------------
+// var name = "superman";
+// var team = "Tapaswini"
+// cap.petersTeam.call()
+
+
+// Related to prototype.--------------------------------
+// let arr=[] -> Array
+// let obj ={} -> Object
+// function fn(){} -> Functions
